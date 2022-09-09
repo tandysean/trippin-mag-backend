35680a0530e7e2580fd7c1411fffd07c7f665513
Tandy Sean <tandysean@gmail.com>
2022-09-09 16:48:24 -0400

    initial commit

---
+++ src/api/taxonomy/controllers/taxonomy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * taxonomy controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::taxonomy.taxonomy');
---
+++ src/api/taxonomy/routes/taxonomy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * taxonomy router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::taxonomy.taxonomy');
